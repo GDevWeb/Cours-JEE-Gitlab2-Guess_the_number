8325bf66339d50405835416cba7d6fe492775cc3
Gaëtan <zodiaquestar@gmail.com>
2025-03-10 13:36:20 +0100

    test => ok

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int targetNumber = random.nextInt(100)+1;
+        Scanner scanner = new Scanner(System.in);
+        int guess = 0;
+        int attempts = 0;
+
+
+        System.out.print("Choississez un nombre entre 0 et 100: ");
+        System.out.println("J'ai choissi " + guess + "."+ "Peux-tu le deviner?");
+
+        while (guess != targetNumber) {
+            System.out.print("Entrez un nombre: (ou tapez 0 pour quitter)");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if(guess == 0){
+                System.out.println("❌ Game over! Vous avez choissi de quitter la partie!");
+                break;
+            }else if(guess < targetNumber){
+                System.out.println("⬆️ Trop bas! Essayes encore.");
+            }else if(guess > targetNumber){
+                System.out.println("⬇️ Trop haut! Essayes encore.");
+            }else{
+                System.out.println("🎉Bravo:! Tu as trouvé le bon nombre " + guess + ".");
+            }
+        }
+        scanner.close();
+    }
+
+}
